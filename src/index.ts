ab6bb1c10ea4b8bd64690482e87fb67cd955bbb0
Samuel Neff <sam@serndesign.com>
2024-07-10 14:26:38 -0400

    First theoretically complete version; untested

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,98 @@
-import { extractEmbeddedTags } from './internal.js';
+import { createMultiProxy, toRecord } from 'utikity';
+import {
+  entityEncode,
+  extractAttributesAndContent,
+  formatAttribute,
+  identity,
+} from './internal.js';
+import {
+  elementDefaultAttributes,
+  htmlAttributes,
+  htmlElements,
+  svgAttributes,
+  svgElements,
+} from './constants.js';
 
-export enum TagFunctionStyle {
-  elements = 'elements',
-  classes = 'classes',
+const htmlElementTagFunctions = createElementTagFunctions(htmlElements);
+const htmlAttributeTagFunctions = createAttributeTagFunctions(htmlAttributes);
+const svgElementTagFunctions = createElementTagFunctions(svgElements);
+const svgAttributTagFunctions = createAttributeTagFunctions(svgAttributes);
+
+export const tagFunctions = {
+  // there is some overlap but the implementations are the same so no worries about the overrides
+  ...svgElementTagFunctions,
+  ...svgAttributTagFunctions,
+  ...htmlElementTagFunctions,
+  ...htmlAttributeTagFunctions,
+};
+
+export function wrapValues<T extends object>(values: T, classNames?: string[], includeDefaults: boolean = true) {
+  const appliedTagFunctions = classNames
+    ? createClassTagFunctions(classNames, includeDefaults)
+    : tagFunctions;
+
+  return createMultiProxy(values, appliedTagFunctions) as unknown as T;
 }
 
-export interface TagFunctionOptions {
-  classNamePrefix?: string;
-  customClassNames?: string[];
-  linksTarget?: string;
-
+function createClassTagFunction(tag: string) {
+  return function classTagFuction(chunks: string[]) {
+    return `<span class="${ tag }">${ entityEncode(chunks.join('')) }</span>`;
+  };
 }
 
-export const specialTagFunctions = {
-  a(chunks: string[]) {
-    const text = chunks.join('');
+export function createClassTagFunctions(classNames: string[], includeDefaults:boolean = true) {
+  const classTagFunctions = toRecord(
+    classNames,
+    identity,
+    createClassTagFunction
+  );
 
+  if (!includeDefaults) {
+    return classTagFunctions;
   }
+
+  return {
+    ...classTagFunctions,
+    ...tagFunctions,
+  };
 }
 
-export function createTagFunction(tag: string) {
-  return function tagFunction(chunks: string[]) {
-    const { text, ...attributes } = extractEmbeddedTags(chunks.join(''));
-
-
-  }
+function createAttributeTagFunction(tag: string) {
+  return function attributeTagFunction(chunks: string[]) {
+    return formatAttribute(tag, chunks.join(''));
+  };
 }
-// export const defaultTagFunctions = createTagFunctions
+
+export function createAttributeTagFunctions(tags: string[]) {
+  return toRecord(
+    tags,
+    identity,
+    createAttributeTagFunction,
+  );
+}
+
+function createElementTagFunction(tag: string, defaultAttributes?: Record<string, string>) {
+  return function elementTagFunction(chunks: string[]) {
+    let {attributes, content} = extractAttributesAndContent(chunks.join(''));
+
+    if (defaultAttributes) {
+      Object.entries(defaultAttributes).forEach(
+        ([ name, value ]) => {
+          if (attributes.includes(` ${ name }="`)) {
+            return;
+          }
+          attributes += formatAttribute(name, value);
+        }
+      );
+    }
+    return `<${ tag }${ attributes }>${ entityEncode(content) }</${ tag }`;
+  };
+}
+
+export function createElementTagFunctions(tags: string[]) {
+  return toRecord(
+    tags,
+    identity,
+    tag => createElementTagFunction(tag, elementDefaultAttributes[tag])
+  );
+}
